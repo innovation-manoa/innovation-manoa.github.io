290675646c68d83919cbde9d027f3aa2880c2dd0
Brendt McFeeley <brendtmcfeeley@gmail.com>
2017-04-05 22:40:18 -1000

    Addition of page mockup

---
+++ app/imports/ui/pages/index.js
@@ -0,0 +1,13 @@
+import './home-page.html';
+
+import './view-all-page.html';
+import './view-all-page.js';
+
+import './add-request-page.html';
+import './add-request-page.js';
+
+import './browse-menu-page.html';
+import './browse-menu-page.js';
+
+import './about-us-page.html';
+import './about-us-page.js';
